844c7e6a10b1ecd8341e7414bd54d68483e49d38
Anmol1696 <anmol1696@gmail.com>
2017-09-02 14:57:56 +0530

    Header for server side code from frontend to msg queue

---
+++ server_for_msg_queue.h
@@ -0,0 +1,22 @@
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <uuid/uuid.h>
+
+// For server
+#include <netdb.h>
+
+// For testing 
+#include "../message_queue/msg_queue.h"
+
+#define PORT 5000
+//#define UUID_SIZE_FOR_STR 32
+
+int init_socket();
+void send_uuid_str_to_msg_queue(char *uuid_str);
