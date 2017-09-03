13e15296cd67a383fc35ba56825245def41cb636
Anmol1696 <anmol1696@gmail.com>
2017-09-03 20:43:45 +0530

    Using writing file from serverside client

--- server_for_backend.c
+++ server_for_backend.c
@@ -1,4 +1,5 @@
 #include "server_for_backend.h"
+#include "serverside_for_clients.h"
 
 int init_socket(){
     int fd =0;
@@ -21,51 +22,79 @@ int init_socket(){
    
     return fd;
 }
+/*
+void concatinate_string(char* filename, char* dir, char* uuid, char* ext){
+    strcpy(filename, dir);
+    strcat(filename, uuid);
+    strcat(filename, ext);
+}*/
 
 void server_send_json_file_to_backend(){
     
     int fd = init_socket();
     int confd, b=0, tot=0;
 
-    char sendbuffer[BUF_SIZE];
-    memset(sendbuffer, '0', sizeof(sendbuffer));
+    char buffer[BUF_SIZE];
+    memset(buffer, '0', sizeof(buffer));
 
     //INIT for UUID
     char* uuid_str = malloc(UUID_SIZE_FOR_STR);
     char filename[strlen(uuid_str) + strlen(DATA_DIR) + strlen(".json") + 1];
+    char output_filename[strlen(uuid_str) + strlen(DATA_DIR) + strlen(".txt") + 1];
     size_t filename_size = strlen(filename);
+    size_t output_filename_size = strlen(output_filename);
     
+    char mode_json_or_result;
+
     while(1){
         confd = accept(fd, (struct sockaddr*)NULL, NULL);
         if (confd==-1) {
             perror("Accept");
             continue;
         }
+        
+        //Getting Mode
+        if((b = read(confd, &mode_json_or_result, 1)) != 1)
+            printf("\nMode specified worngly\n\n");
+        
         //Getting UUID
         if((b = recv(confd, uuid_str, UUID_SIZE_FOR_STR, 0)) < UUID_SIZE_FOR_STR)
             printf("UUID less then required size\n");
          
         printf("UUID recived -> %s\n", uuid_str); 
-        strcpy(filename, DATA_DIR);
-        strcat(filename, uuid_str);
-        strcat(filename, ".json");
-        printf("Filename -> %s and len-> %d\n", filename, strlen(filename));
         
-        FILE* fp = fopen(filename, "rb");
-        tot=0;
-        if(fp != NULL){
-            while((b = fread(sendbuffer, 1, sizeof(sendbuffer), fp)) > 0){
-                send(confd, sendbuffer, b, 0);
+        if(mode_json_or_result == 'j'){
+            printf("\nThis is json file giver\n");
+            strcpy(filename, DATA_DIR);
+            strcat(filename, uuid_str);
+            strcat(filename, ".json");
+            printf("Filename -> %s and len-> %d\n", filename, strlen(filename));
+        
+            FILE* fp = fopen(filename, "rb");
+            tot=0;
+            if(fp != NULL){
+                while((b = fread(buffer, 1, sizeof(buffer), fp)) > 0){
+                    send(confd, buffer, b, 0);
+                }
+                if (fclose(fp)) { printf("error closing file."); exit(-1); }
+                printf("No error in closing file or sending file\n");
+            } else {
+                //perror("File");
+                printf("ERROR in FILE\n");
             }
-            if (fclose(fp)) { printf("error closing file."); exit(-1); }
-            printf("No error in closing file or sending file\n");
-        } else {
-            //perror("File");
-            printf("ERROR in FILE\n");
-        }
 
-        printf("UUID Recived -> %s\n", uuid_str);
-         
+            printf("UUID Recived -> %s\n", uuid_str);
+        }
+        else if(mode_json_or_result == 'r'){
+            printf("\nThis will be storing files\n");
+            strcpy(output_filename, DATA_DIR);
+            strcat(output_filename, uuid_str);
+            strcat(output_filename, ".txt");
+            printf("Output Filenam -> %s\n", output_filename);
+
+            write_to_file_from_client(confd, buffer, output_filename); 
+            printf("Rsult Stored on frontend\n");
+        }
         close(confd);
     }
 
